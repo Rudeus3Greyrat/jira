bf9a32f571cbed7244ce460fe48f25097d5562c4
meiziwen1 <meiziwen1@xdf.cn>
2021-11-16 16:07:27 +0800

    feat: 登录注册和错误处理

--- src/utils/useAsync.ts
+++ src/utils/useAsync.ts
@@ -11,7 +11,14 @@ const defaultInitialState: State<null> = {
   data: null,
   error: null,
 };
-export const useAsync = <D>(initialState?: State<D>) => {
+const defaultConfig = {
+  throwOnError: false,
+};
+export const useAsync = <D>(
+  initialState?: State<D>,
+  initialConfig?: typeof defaultConfig
+) => {
+  const config = { ...defaultConfig, ...initialConfig };
   const [state, setState] = useState<State<D>>({
     ...defaultInitialState,
     ...initialState,
@@ -44,7 +51,9 @@ export const useAsync = <D>(initialState?: State<D>) => {
         return data;
       })
       .catch((error) => {
+        //catch会消化异常，如果不主动抛出
         setError(error);
+        if (config.throwOnError) return Promise.reject(error);
         return error;
       });
   };
