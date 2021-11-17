2342631d1ec1ca6c990ccbc4946b790d526afe2d
meiziwen1 <meiziwen1@xdf.cn>
2021-11-17 10:37:05 +0800

    feat: 添加路由

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -3,6 +3,7 @@ import { User } from "screens/project-list/search-panel";
 import { Table } from "antd";
 import dayjs from "dayjs";
 import { TableProps } from "antd/es/table";
+import { Link } from "react-router-dom";
 
 export interface Project {
   id: string;
@@ -25,8 +26,10 @@ export const List = ({ users, ...props }: ListProps) => {
       columns={[
         {
           title: "名称",
-          dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={project.id.toString()}>{project.name}</Link>;
+          },
         },
         {
           title: "部门",
