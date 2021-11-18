c2187404dce8ca22786f7e758c0caf9272c6feaf
meiziwen1 <meiziwen1@xdf.cn>
2021-11-18 15:24:41 +0800

    feat: 使用component composition实现modal状态共享

---
+++ src/components/project-popover.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Divider, List, Popover, Typography } from "antd";
+import { useProjects } from "utils/project";
+import styled from "@emotion/styled";
+
+export const ProjectPopover = (props: { projectButton: JSX.Element }) => {
+  const { data: projects, isLoading } = useProjects();
+  const pinnedProjects = projects?.filter((project) => project.pin);
+
+  const content = (
+    <ContentContainer>
+      <Typography.Text type={"secondary"}>收藏项目</Typography.Text>
+      <List>
+        {pinnedProjects?.map((project) => (
+          <List.Item key={project.id}>
+            <List.Item.Meta title={project.name} />
+          </List.Item>
+        ))}
+      </List>
+      <Divider />
+      {props.projectButton}
+    </ContentContainer>
+  );
+
+  return (
+    <Popover placement={"bottom"} content={content}>
+      <span>项目</span>
+    </Popover>
+  );
+};
+
+const ContentContainer = styled.div`
+  min-width: 30rem;
+`;
